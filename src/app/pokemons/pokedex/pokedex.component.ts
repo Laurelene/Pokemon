a8b44f19bdc3fe65e930740ba61cf79176ec0d4f
Laurelene <58777491+Laurelene@users.noreply.github.com>
2023-03-08 16:02:28 +0100

    TP2 fin, manque style

---
+++ src/app/pokemons/pokedex/pokedex.component.ts
@@ -0,0 +1,33 @@
+import {Component, Output} from '@angular/core';
+import {PokemonListComponent} from "../pokemon-list/pokemon-list.component";
+import {PokemonDetailComponent} from "../pokemon-detail/pokemon-detail.component";
+import {PokemonService} from "../../services/pokemon.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Pokemon} from "../../models/pokemon.model";
+
+@Component({
+  selector: 'app-pokedex',
+  templateUrl: './pokedex.component.html',
+  styleUrls: ['./pokedex.component.css']
+})
+export class PokedexComponent {
+
+  pokemonsList?: PokemonListComponent;
+
+  pokemonDetail?: number;
+
+ /* constructor(
+    private router: Router
+  ) { }
+
+  // pour que le refresh avec l'id du pokemon selectionné se fasse
+  // ne pas le faire comme ça mais avec des input et des output
+  public ngOnInit(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }*/
+
+  selectPokemon(pokemon: number) {
+    return this.pokemonDetail = pokemon;
+  }
+
+}
